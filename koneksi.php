3b43ace824a18d91abbe4b15c057c3a379e5fc65
badrei7 <badrey774@gmail.com>
2024-02-19 10:31:06 +0700

    mengubah beberapa dibagian koneksi database

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php 
-    $con = mysqli_connect("localhost", "root", "", "toko_online");
+    $con = mysqli_connect("172.17.0.3", "root", "alba", "toko_online");
 
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
